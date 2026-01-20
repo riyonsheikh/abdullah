ef942382bf1347889c7703d2b3d35a0a11d8d89d
Abdullah <bd525784@gmail.com>
2026-01-20 10:59:10 +0600

    fao

--- python.py
+++ python.py
@@ -7,3 +7,13 @@ if guess == secret:
     print("Correct! You guessed the number!")
 else:
     print(f"Wrong! The number was {secret}")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Sum =", a + b)
+print("Sub =", a - b)
+print("Mul =", a * b)
+
+if b != 0:
+    print("Div =", a / b)
+else:
+    print("Cannot divide by zero")
