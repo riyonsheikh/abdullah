47188fd43debe31fad107c8eceecc81f6128cc32
Abdullah <bd525784@gmail.com>
2026-01-20 11:07:27 +0600

    add at web

--- python.py
+++ python.py
@@ -115,3 +115,16 @@ def game_loop():
 
 # Run the game
 game_loop()
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is Even")
+else:
+    print(num, "is Odd")
+
+if num > 0:
+    print("Number is Positive")
+elif num < 0:
+    print("Number is Negative")
+else:
+    print("Number is Zero")
